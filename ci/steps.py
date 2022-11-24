4aae188daaa0644f983c464a5f1d88a55001050b
Christian Cwienk <christian.cwienk@sap.com>
2022-11-24 21:42:41 +0100

    rm (now unused) kaniko-image

--- ci/steps.py
+++ ci/steps.py
@@ -11,7 +11,6 @@ import tkn.model
 
 IMAGE_VERSION = '1.1878.0'
 DEFAULT_IMAGE = f'eu.gcr.io/gardener-project/cc/job-image:{IMAGE_VERSION}'
-KANIKO_IMAGE = f'eu.gcr.io/gardener-project/cc/job-image-kaniko:{IMAGE_VERSION}'
 CACHED_PATCH: str = None
 
 logger = logging.getLogger(__name__)
