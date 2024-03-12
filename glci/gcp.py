965d54a5cd9a1ecc855790b34acc251c01fda9d5
Thomas Buchner <thomas.buchner@sap.com>
2024-03-12 12:45:43 +0100

    fix specifying guest os features (#56)
    for an imges.insert() request, if a raw body is supplied, the guestOsFeatures expect the key type (not type_ as if an imageConfig got supplied instead)

--- glci/gcp.py
+++ glci/gcp.py
@@ -104,13 +104,13 @@ def insert_image_to_gce_image_store(
             },
             'guestOsFeatures': [
                 {
-                    'type_': 'VIRTIO_SCSI_MULTIQUEUE',
+                    'type': 'VIRTIO_SCSI_MULTIQUEUE',
                 },
                 {
-                    'type_': 'UEFI_COMPATIBLE',
+                    'type': 'UEFI_COMPATIBLE',
                 },
                 {
-                    'type_': 'GVNIC',
+                    'type': 'GVNIC',
                 },
             ],
         },
